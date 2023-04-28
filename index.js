3ef7726a4083b9c6255120660a51efdbf9c1a894
elsie <elsiechen915@gmail.com>
2023-04-28 11:22:16 -0400

    Console version of game without win condition

--- index.js
+++ index.js
@@ -1,23 +1,92 @@
-var content = document.querySelector('.content');
+function gameBoard () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
-const gameBoard = (() => {
-    let gameBoard = ['X', 'O'];
-    const renderGameBoard = () => {
-        for(let i=0; i<gameBoard.length; i++){
-            const div = document.createElement('div');
-            div.textContent = gameBoard[i];
-            content.appendChild(div);
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            // Important: push object cell() instead of value 0
+            board[i].push(cell());
         }
+    }
+
+    const getBoard = () => board;
+
+    const putMarker = (row, column, playerMarker) => {
+        board[row][column].addMarker(playerMarker);
     };
-    return { renderGameBoard };
-})();
-// render the contents of the gameboard array to the webpage 
-gameBoard.renderGameBoard();
 
-// const Player = () => {
+    const printBoard = () => {
+        const boardWithCellValues = 
+            board.map(row => row.map(cell => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
 
-// };
+    return { getBoard, putMarker, printBoard };
+}
 
-// const displayController = (() => {
 
-// })();
+//  A Cell represents one "square" on the board and can have one of
+//  0: no token is in the square,
+//  1: Player One's token,
+//  2: Player 2's token
+
+function cell() {
+    let value = 0;
+
+    const addMarker = playerMarker => {
+        value = playerMarker;
+    };
+
+    const getValue = () => value;
+
+    return { addMarker, getValue };
+}
+
+function gameController(playerOne, playerTwo) {
+    const board = gameBoard();
+
+    const players = [
+        {
+            name: playerOne,
+            marker: 1
+        },
+        {
+            name: playerTwo,
+            marker: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0]? 
+            players[1]: players[0];
+    }; 
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${ getActivePlayer().name }'s turn`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`Put ${ getActivePlayer().name }'s marker on 
+            row ${ row } and column ${ column }`);
+        
+        board.putMarker(row, column, getActivePlayer().marker);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+    // Initial play game message
+    printNewRound();
+
+    return { getActivePlayer, playRound};
+}
+
+const game = gameController('playerOne', 'playerTwo');
+
+// game();
